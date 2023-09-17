44a5bdb2c23b7cdb1f08b23d67a30dfa6ec6df2c
Ben Chen <77423140+bennychen2004181026@users.noreply.github.com>
2023-09-17 17:53:00 +1000

    Add filter query logic

--- router.js
+++ router.js
@@ -3,26 +3,36 @@ const express = require("express");
 const router = express.Router();
 
 const data = [
-    {
-      id: 1,
-      description: 'task 1',
-      done: false,
-    },
-    {
-      id: 2,
-      description: 'task 2',
-      done: false,
-    },
-    {
-      id: 3,
-      description: 'task 3',
-      done: false,
-    },
-  ];
+  {
+    id: 1,
+    description: "task 1",
+    done: false,
+  },
+  {
+    id: 2,
+    description: "task 2",
+    done: false,
+  },
+  {
+    id: 3,
+    description: "task 3",
+    done: false,
+  },
+];
 
-  router.get('/tasks',(req,res)=>{
-    console.log(1)
-    res.send(data)
-  })
+router.get("/tasks", (req, res) => {
+  const { description } = req.query;
+  if (description) {
+    console.log(description);
+    const filtered = data.filter((task) =>
+      task.description.includes(description)
+    );
+    console.log(filtered);
+    res.json(filtered);
+    return;
+  }
+  res.json(data);
+  // return
+});
 
-  module.exports = router;
+module.exports = router;
